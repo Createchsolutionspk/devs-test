30c76e1d565007055a52133570344a7d44901bcb
shahSpider <aq.syed007@gmail.com>
2025-01-22 12:25:52 +0500

    add test files

---
+++ script.py
@@ -0,0 +1,108 @@
+"""
+Task 1
+
+Manipulate the data for in_qty and out_qty
+- Group by Warehouse
+	- Group by Item Group
+		- Group by Item
+	
+input: stock-balance-dataset.json
+Output: stock-balance.csv
+
+
+"""
+
+
+
+
+
+
+
+
+
+"""
+Task 2
+Write an SQL query to retrieve data from three related tables: Sales Invoice, Sales Invoice Item, and Customer. The query should perform the following inner joins:
+
+    Join Sales Invoice with Sales Invoice Item on the invoice number.
+    Join the result with the Customer table on the customer ID.
+
+The query should select the following fields:
+
+    posting_date from the Sales Invoice table.
+    item_code and qty from the Sales Invoice Item table.
+    mobile_number from the Customer table.
+"""
+
+
+SQL_QUERY = """
+
+"""
+
+
+
+
+
+
+"""
+Task 3
+
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+"""
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+    	pass
+
+
+
+
+
+
+
+
+
+
+"""
+Task 4
+
+Write a class of animal, give it attributes of name, age
+
+write a animal sound method
+
+write another class and inherit parent class and override the animal voice method in child class
+
+"""
+
+
+
+
+
+
+
+"""
+Task 5
+
+Write any python decorator
+
+"""
